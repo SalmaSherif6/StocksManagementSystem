68fab6890e9888923a6c0cabb90ec955d00090e8
Salma Sherif <50235031+SalmaSherif6@users.noreply.github.com>
2023-06-10 14:22:19 +0300

    Add seed data for stocks

--- StocksManagementSystem.API/Program.cs
+++ StocksManagementSystem.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using StocksManagementSystem.Infrastucture.DbContexts;
+using StocksManagementSystem.Infrastucture.Repositories;
+using StocksManagementSystem.Services.Interfaces;
+using StocksManagementSystem.Services.Repositories;
+using StocksManagementSystem.Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
+builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 var app = builder.Build();
 
@@ -28,3 +37,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+app.UseRouting();
---
+++ StocksManagementSystem.Infrastucture/Migrations/20230610102211_SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StocksManagementSystem.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Stocks",
+                columns: new[] { "Name", "Price" },
+                values: new object[,]
+                {
+                    { "Vianet", 10.5 },
+                    { "Agritek", 20.5 },
+                    { "Akamai", 50 },
+                    { "Baidu", 100 },
+                    { "Blinkx", 10 },
+                    { "Blucora", 172.5 },
+                    { "Boingo", 10.5 },
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
